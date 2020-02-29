f9e234f74107ea6df3411a5c16ea367b57362ba9
soundsnick <soundsnick@gmail.com>
2020-02-29 23:56:52 +0600

    asd g

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Upmind
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    self.allow_forgery_protection = false
+    config.api_only = false
     config.assets.paths << Rails.root.join('app/assets/fonts')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
